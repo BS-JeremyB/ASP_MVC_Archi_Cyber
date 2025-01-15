3ec57b300b3cc7acbec4b06944b8cc48ed2070bc
BS-JeremyB <jeremy.bazin@bstorm.be>
2025-01-15 12:19:18 +0100

    Intro ADO

--- ASP_MVC_Archi_Cyber/Controllers/HomeController.cs
+++ ASP_MVC_Archi_Cyber/Controllers/HomeController.cs
@@ -1,29 +1,55 @@
 using ASP_MVC_Archi_Cyber.Models;
+using Elfie.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ASP_MVC_Archi_Cyber.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string connectionString = "Data Source = (localdb)\\BStormLocalDB; Initial Catalog = db_asp_archi; Integrated Security = True; Connect Timeout = 60; Encrypt = True; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                ViewBag.Connection = conn.State;
+
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT GETDATE() as DATE";
+                    cmd.CommandType = CommandType.Text;
+
+                    DateTime dateServer = (DateTime)cmd.ExecuteScalar();
+                    conn.Close();
+
+                    ViewData["Date"] = dateServer;
+
+                }
+            }
+
+            // ExecuteScalar - Permet de récupérer une seule cellule (equivalent de A-1
+            // ExecuteNonQuery - Permet de récupérer le nombre de lignes affectées
+            // Reader - Permet de récupérer des lignes entières
+
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-
-        public IActionResult AboutUs() 
+        public IActionResult AboutUs()
         {
             List<string> team = new List<string> { "John Doe", "Jane Doe", "Steve Lorent", "Donald Trump", "Joe Biden" };
             // retours de db
